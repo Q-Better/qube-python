7c99e410b641082fa36b664ed39feed76486c86f
João Ferreira <joao.ferreira@q-better.com>
2024-11-14 14:36:26 +0000

    [Refactor] Change Rest client methods
    - Add new arguments to Rest client methods
    - Change documentation
    
    Tasks: [T-6324]

--- qube/rest/clients.py
+++ qube/rest/clients.py
@@ -5,12 +5,21 @@ from qube.rest.queue_management import QueueManagementManager
 
 class RestClient:
     """
-    Client class to store some attributes needed to make requests to Qube
+    Client class for storing some attributes needed to make requests to Qube, getting queue management manager
+    (default or not) and making some requests to API Server.
     """
 
     API_BASE_URL = "api.qube.q-better.com"
 
     def __init__(self, api_key: str, location_id: int, queue_management_manager: object = None, base_url: str = None):
+        """
+        Initializes and connects the Rest client.
+        Args:
+            api_key (str): API key for client authentication.
+            location_id (int): Location's id that will be used in requests.
+            queue_management_manager (object, optional): Manager used on API Server interactions. Defaults to None.
+            base_url (str, optional): Base url used on API interactions . Defaults to API_BASE_URL.
+        """
         self.base_url = base_url or self.API_BASE_URL
         self.api_key = api_key
         self.headers = {
@@ -29,23 +38,34 @@ class RestClient:
 
         return self.queue_management_manager
 
-    def get_request(self, path: str) -> object:
+    def get_request(self, path: str, params: dict = None) -> object:
         """
-        Makes a GET request to api. This method can be useful for Managers.
+        Makes a GET request to API Server. This method can be useful for Managers.
+        Args:
+            path (str): Path of URL to be added to base url to make the request.
+            params (dict): Query parameters that will be included in the URL.
         """
-        response = requests.get(self.base_url + path, headers=self.headers, timeout=10)
+        response = requests.get(self.base_url + path, headers=self.headers, params=params, timeout=10)
         return response
 
-    def post_request(self, path: str, data: dict) -> object:
+    def post_request(self, path: str, params: dict = None, data: dict = None) -> object:
         """
-        Makes a POST request to api. This method can be useful for Managers.
+        Makes a POST request to API Server. This method can be useful for Managers.
+        Args:
+            path (str): Path of URL to be added to base url to make the request.
+            params (dict): Query parameters that will be included in the URL.
+            data (dict): Data that will be sent in the body of the request.
         """
-        response = requests.post(self.base_url + path, headers=self.headers, data=data, timeout=10)
+        response = requests.post(self.base_url + path, headers=self.headers, params=params, data=data, timeout=10)
         return response
 
-    def put_request(self, path: str, data: dict) -> object:
+    def put_request(self, path: str, params: dict = None, data: dict = None) -> object:
         """
-        Makes a PUT request to api. This method can be useful for Managers.
+        Makes a PUT request to API Server. This method can be useful for Managers.
+        Args:
+            path (str): Path of URL to be added to base url to make the request.
+            params (dict): Query parameters that will be included in the URL.
+            data (dict): Data that will be sent in the body of the request.
         """
-        response = requests.put(self.base_url + path, headers=self.headers, data=data, timeout=10)
+        response = requests.put(self.base_url + path, headers=self.headers, params=params, data=data, timeout=10)
         return response
